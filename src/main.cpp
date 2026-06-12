15ccd03b2065976ac84cd21d132e2ad8703d6e30
Ronaldo <ronaldoanthony12@gmail.com>
2026-06-12 18:58:12 -0400

    Finish Temp

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,39 @@ void getData() {
     json += "\"temperature\": ";
     json += String(temp, 2);
     json += ", \"humidity\": ";
-    json += "60%}";
+    json += "60}";
     server.send(200, "application/json", json);
 }
 
 void handleRoot() {
-    server.send(200, "text/html", "<h1>Hello</h1>");
-    
+    String html = R"rawliteral(
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Sensor Dashboard</title>
+    </head>
+    <body>
+        <h1>Wireless Sensor Station</h1>
+
+            <p>Temperature: <span id="temp">Loading...</span> °C</p>
+
+    <script>
+        async function updateData() {
+            const response = await fetch('/data');
+            const data = await response.json();
+
+            document.getElementById('temp').textContent =
+                data.temperature;
+        }
+
+        updateData();
+        setInterval(updateData, 500);
+    </script>
+    </body>
+    </html>
+    )rawliteral";
+
+    server.send(200, "text/html", html);
 }
 
 void setup() {
@@ -46,8 +72,9 @@ void setup() {
         Serial.print("Your link: http://"); Serial.print(WiFi.localIP()); Serial.println("/data");
         
         //firing up the server
-        server.begin();
+        server.on("/", handleRoot);
         server.on("/data", getData);
+        server.begin();
 
     }
     else {
