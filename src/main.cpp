4664dd9e0b9522da2449bee07059ccf3d2ef658a
Ronaldo <ronaldoanthony12@gmail.com>
2026-06-13 21:33:33 -0400

    Refactor handleroot

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 #include "thermsistor.h"
 #include "photoresistor.h"
 #include "distSensor.h"
+#include <SPIFFS.h>
 
-const char* ssid = "1051A";
-const char* password = "Secord1051A";
+const char* ssid = "wifi";
+const char* password = "pass";
 
 WebServer server(80);  // Create a web server on port 80
 
