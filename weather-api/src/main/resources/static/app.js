a7ab476815452727c228573f0301bd84b524af20
Ronaldo <ronaldoanthony12@gmail.com>
2026-06-15 22:13:03 -0400

    Fix JS

--- weather-api/src/main/resources/static/app.js
+++ weather-api/src/main/resources/static/app.js
@@ -1,5 +1,5 @@
 async function updateData() {
-    const response = await fetch('/data'); //ask esp for latest Sensor  values
+    const response = await fetch('/weather'); //ask esp for latest Sensor  values
     const data = await response.json(); //
 
     document.getElementById('temp').textContent = data.temperature + "\u00B0C";
