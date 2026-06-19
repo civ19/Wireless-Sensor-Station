1200fd9208842fcf21067db0bda22907cecd6e61
_ronaldo <ronaldoanthony12@gmail.com>
2026-06-19 19:32:10 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 #include "distSensor.h"
 #include <SPIFFS.h>
 
-const char* ssid = "1051A";
-const char* password = "Secord1051A";
+const char* ssid = "wifi";
+const char* password = "pass";
 
 WebServer server(80);  // Create a web server on port 80
 
